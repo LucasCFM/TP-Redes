55b82afb5e7dea1e78d8aa72a8834305f99aa7b5
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2020-06-10 00:09:20 -0300

    Removed retransmission on server UDP Connector, left only on Client side

--- app/server/udp_connector.py
+++ app/server/udp_connector.py
@@ -31,14 +31,6 @@ class Connector():
         UDPServerSocket.bind( (server_ip, server_port) )
 
 
-    def change_server(self, server_ip: str, server_port: int):
-        global UDPServerSocket
-        
-        self.server_address = (server_ip, server_port)
-        UDPServerSocket.bind( self.server_address )
-        
-        print(f'Server changed to {self.server_address}')
-
     '''
     settimeout
     https://stackoverflow.com/questions/2719017/how-to-set-timeout-on-pythons-socket-recv-method
@@ -48,7 +40,9 @@ class Connector():
         UDPServerSocket.settimeout(timeout)
 
 
-    def __send(self, byte_msg: bytearray, destiny_address: tuple):
+    def send_msg(self, byte_msg: bytearray, destiny_address: tuple):
+        
+        
         global UDPServerSocket
         self.set_timeout(3.0)
 
@@ -79,7 +73,7 @@ class Connector():
 
         return bytes_received
     
-    
+
     def send_msg_received_confirmation(self, address : tuple):
         print(f'Sending confirmation message')
         confirmation_msg = {
@@ -87,44 +81,6 @@ class Connector():
         }
         confimationByteMsg : bytearray = json_to_byte( confirmation_msg )
         print(f'Sending ...')
-        self.__send( confimationByteMsg, address )
+        self.send_msg( confimationByteMsg, address )
         print(f'Confirmation message sent!')
-
-
-    def send_msg(self, byte_msg: bytearray, destiny_address: tuple):
-        """ Public method to send messages
-        Implements retransmission of messages, sending another message if the first fails
-        only returns False if both of them don't send properly 
-
-        param: byte_message : byte representation of the message string
-        param: destiny_address : address tuple that the message will be sent
-        returns: bool : If message was successfully sent or it faild
-        """
-        
-        print(f'Sending message')
-        try:
-            self.__send( byte_msg, destiny_address )
-        except Exception:
-            print(f'Could not send message properly')
-            print(f'Retrying ....')
-
-            try:
-                self.__send( byte_msg, destiny_address )
-            except Exception:
-                print(f'Could not send message properly')
-                print(f'Aborted !!')
-                return False
-        
-        try:
-            byte_rsp = self.get_message()
-        except Exception:
-            print(f'Could not get response properly')
-            print(f'Aborted')
-            return False
-        
-        json_data = byte_to_json( byte_rsp ) # ERROR - Maybe does not need to receive response on line 97 (up here)
-        if json_data['success'] == True:
-            return True
-        
-        return False
 
